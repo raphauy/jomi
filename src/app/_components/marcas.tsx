04d514c4d967b033aaa2950f1fab5a46368e619b
raphauy <rapha.uy@rapha.uy>
2023-11-13 07:59:32 -0300

    v5

--- src/app/_components/marcas.tsx
+++ src/app/_components/marcas.tsx
@@ -10,7 +10,7 @@ export default async function Marcas() {
   const marcas= await getMarcasDAO()
   return (
     <section id="marcas" className="bg-white py-8 flex flex-col items-center justify-center min-h-[600px] px-1">
-      <h2 className={cn(helveticaMedium.className, "text-3xl sm:text-4xl mb-5")}>MARCAS REPRESENTADAS</h2>
+      <h2 className={cn(helveticaMedium.className, "text-3xl sm:text-4xl mb-5 text-center")}>MARCAS REPRESENTADAS</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-2 md:gap-4 max-w-5xl">
         {
           marcas.map((marca) => (
