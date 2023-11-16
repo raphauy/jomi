f2af295f84ce0c133af484041cf8ed23f117ea64
raphauy <rapha.uy@rapha.uy>
2023-11-16 14:02:49 -0300

    small change

--- src/app/rubros/[rubroId]/page.tsx
+++ src/app/rubros/[rubroId]/page.tsx
@@ -20,7 +20,7 @@ export default async function RubroPage({ params }: Props) {
     
 
     return (
-        <section className={cn(helveticaLight.className, "fondo-cheers relative w-full h-screen pt-20 text-white flex flex-col items-center gap-10 text-center bg-cover")} >
+        <section className={cn(helveticaLight.className, "fondo-cheers z-0 relative w-full h-screen pt-20 text-white flex flex-col items-center gap-10 text-center bg-cover")} >
 
             <div className="absolute inset-0 bg-black opacity-50" />
             <div className="z-0 py-20">
